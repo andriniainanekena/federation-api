ab45dd96e6f816b0fe1fa292e1bbc304846889c3
Nekena Andriniaina <andriniainanekena@gmail.com>
2026-04-21 13:13:00 +0300

    Add default structure for the project

---
+++ src/main/java/federation/agricole/api/FederationApiApplication.java
@@ -0,0 +1,14 @@
+package federation.agricole.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FederationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FederationApiApplication.class, args);
+	}
+
+}
+
