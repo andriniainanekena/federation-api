0cbac22dec24cb50240239908a37b487cbf44380
Ismael <hei.ismael.3@gmail.com>
2026-04-22 08:47:17 +0300

    add collectivity in entity

---
+++ src/main/java/federation/agricole/api/entity/Collectivity.java
@@ -0,0 +1,27 @@
+package federation.agricole.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+
+public class Collectivity {
+    private String id;
+    private String location;
+    private LocalDate dateCreation;
+    private CollectivityStructure structure;
+    private List<Member> members;
+
+    public Collectivity() {
+
+    }
+
+}
