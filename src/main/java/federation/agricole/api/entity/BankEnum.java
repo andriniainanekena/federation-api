9e9c77a39bfecfc3e13e2a2570ede8cc0207a9c8
Ismael <hei.ismael.3@gmail.com>
2026-04-23 08:58:36 +0300

    add enm in entity

---
+++ src/main/java/federation/agricole/api/entity/BankEnum.java
@@ -0,0 +1,4 @@
+package federation.agricole.api.entity;
+public enum BankEnum {
+    BRED, MCB, BMOI, BOA, BGFI, AFG, ACCES_BANQUE, BAOBAB, SIPEM
+}
---
+++ src/main/java/federation/agricole/api/entity/MobileBankingServiceEnum.java
@@ -0,0 +1,2 @@
+package federation.agricole.api.entity;
+public enum MobileBankingServiceEnum { AIRTEL_MONEY, MVOLA, ORANGE_MONEY }
