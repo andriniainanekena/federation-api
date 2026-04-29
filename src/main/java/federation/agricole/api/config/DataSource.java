cb971a348dd0ae7cd68daba2b97748f83d07b02e
Ismael <hei.ismael.3@gmail.com>
2026-04-29 10:40:12 +0300

    chore: update DataSource configuration

--- src/main/java/federation/agricole/api/config/DataSource.java
+++ src/main/java/federation/agricole/api/config/DataSource.java
@@ -16,7 +16,7 @@ public class DataSource {
     @Bean
     public Connection getConnection() {
         try {
-            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/federationapi", "postgres", "postgres");
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/federation_db", "postgres", "police2005");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
